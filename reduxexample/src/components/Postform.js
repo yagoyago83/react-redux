339220ea980256cccc2ffa3c593de1572e2391f2
Yagmyr Musayev <yagmur.cw@gmail.com>
2019-09-24 01:28:30 -0700

    redux example

---
+++ reduxexample/src/components/Postform.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createPost } from '../actions/postActions';
+
+class Postform extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            body: ''
+        };
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+    onChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+
+    }
+    onSubmit(e) {
+        e.preventDefault();
+        const post = {
+            title: this.state.title,
+            body: this.state.body
+        };
+        this.props.createPost(post);
+    }
+    render() {
+        return ( 
+        <div>
+            <h1> Add Post </h1>  
+            <form>
+            <div>
+            <label> Title: </label> <br/ >
+            <input type = "text" name = "title"/>
+            </div><br/ >
+            <div>
+            <label> Body: </label><br/ >
+            <textarea name = "body"/>
+            </div> 
+            <br/>
+            <button type = "submit" > Submit </button> 
+            </form> 
+            </div>
+        )
+    }   
+}
+
+Postform.propTypes = {
+    createPost:PropTypes.func.isRequired
+}; 
+export default connect(null, {createPost})(Postform) ;
